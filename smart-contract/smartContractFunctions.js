fdddeb4d92e838b487648c06ddfd3dbff4956992
Arunava-Barua <rahulbarua31@gmail.com>
2023-02-18 18:37:53 +0530

    Fixes

--- smart-contract/smartContractFunctions.js
+++ smart-contract/smartContractFunctions.js
@@ -7,13 +7,13 @@ const ContractJson = require("./NFTRent.json");
 dotenv.config();
 
 const abi = ContractJson.abi;
-const API_KEY = process.env.API_KEY ;
+const API_KEY_MUMBAI = process.env.API_KEY_MUMBAI ;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const verify = async (_nftHash, _renter) => {
   const alchemy = new hre.ethers.providers.AlchemyProvider(
-    "goerli",
-    API_KEY
+    "maticmum",
+    API_KEY_MUMBAI
   );
 
     const contract = new ethers.Contract(
