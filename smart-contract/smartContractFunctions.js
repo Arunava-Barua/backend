052ae644f277dddfd49303458c6d45092aad1625
Arunava-Barua <rahulbarua31@gmail.com>
2023-02-18 15:34:29 +0530

    API for renting verification

---
+++ smart-contract/smartContractFunctions.js
@@ -0,0 +1,33 @@
+const { ethers } = require("ethers");
+const hre = require("hardhat");
+const dotenv = require("dotenv");
+
+const ContractJson = require("./NFTRent.json");
+
+dotenv.config();
+
+const abi = ContractJson.abi;
+const API_KEY = process.env.API_KEY ;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const verify = async (_nftHash, _renter) => {
+  const alchemy = new hre.ethers.providers.AlchemyProvider(
+    "goerli",
+    API_KEY
+  );
+
+    const contract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      abi,
+      alchemy
+    );
+
+    const txRes = await contract.verify(_nftHash, _renter);
+
+    console.log("Response From contract interaction: ", txRes);
+
+    return txRes;
+  
+};
+
+module.exports = {verify};
