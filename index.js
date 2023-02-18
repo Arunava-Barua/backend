75159f23dd3ba4024a22a7f674a8d088076bda99
Arunava-Barua <rahulbarua31@gmail.com>
2023-02-18 20:27:14 +0530

    Changed response

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const app = express();
 
-const {verify} = require("./smart-contract/smartContractFunctions")
+const { verify } = require("./smart-contract/smartContractFunctions");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/verify/:renter/:nft_hash", async (req, res) => {
-  let {nft_hash, renter} = req.params;
-  nft_hash= nft_hash.toString();
+  let { nft_hash, renter } = req.params;
+  nft_hash = nft_hash.toString();
   renter = renter.toString();
-  res.send(`Verified for renter: ${renter} NFT: ${nft_hash}`);
-
   const result = await verify(nft_hash, renter);
+  res.json({ verified: result });
+
   console.log("Response From API call: ", result);
 
   return result;
 });
- 
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
