5038ef6803773206e8fd0a95320abb6a1f79c638
Arunava-Barua <rahulbarua31@gmail.com>
2023-02-18 21:36:05 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
+
 const { verify } = require("./smart-contract/smartContractFunctions");
 
 app.get("/", (req, res) => {
